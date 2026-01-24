ff5933128872542939b1e137c5b2cdca9358c77b
bjbutterfield25 <194412712+bjbutterfield25@users.noreply.github.com>
2026-01-23 22:40:02 -0700

    Implemented isInCheck method. Passes in Check tests

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -68,7 +69,37 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        // get king location
+        ChessPosition kingLocation = null;
+        for (int i = 1; i < 9; i++){
+            for (int j = 1; j < 9; j++){
+                ChessPosition position = new ChessPosition(i, j);
+                if (Objects.equals(currentBoard.getPiece(position), new ChessPiece(teamColor, ChessPiece.PieceType.KING))){
+                    kingLocation = position;
+                }
+            }
+        }
+        // get all attacking piece locations
+        ArrayList<ChessPosition> pieceLocations = new ArrayList<>();
+        for (int i = 1; i < 9; i++){
+            for (int j = 1; j < 9; j++){
+                ChessPosition position = new ChessPosition(i, j);
+                if (currentBoard.getPiece(position) != null && currentBoard.getPiece(position).getTeamColor() != teamColor){
+                    pieceLocations.add(position);
+                }
+            }
+        }
+        //check if attacking piece can move to kingLocation
+        for (ChessPosition position: pieceLocations){
+            ChessPiece piece = currentBoard.getPiece(position);
+            Collection<ChessMove> pieceMoves = piece.pieceMoves(currentBoard, position);
+            for(ChessMove move: pieceMoves){
+                if(move.getEndPosition().equals(kingLocation)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     /**
