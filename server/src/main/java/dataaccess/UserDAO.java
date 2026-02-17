ca543554dcab5343f6aef77aade6133486c8bda2
bjbutterfield25 <194412712+bjbutterfield25@users.noreply.github.com>
2026-02-16 18:14:08 -0700

    Created basic UserDAO

---
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -0,0 +1,9 @@
+package dataaccess;
+
+import model.UserData;
+
+public interface UserDAO {
+    public void createUser(UserData userData);
+    public UserData getUser(String username);
+    public void clear();
+}
