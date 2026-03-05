a52c7bb657b411fb853fa7bb2ddc438a47fcc0b1
bjbutterfield25 <194412712+bjbutterfield25@users.noreply.github.com>
2026-03-05 13:11:42 -0700

    Implemented getUser method with sql

--- server/src/main/java/dataaccess/SQLUserDao.java
+++ server/src/main/java/dataaccess/SQLUserDao.java
@@ -3,6 +3,10 @@ package dataaccess;
 import model.UserData;
 import org.mindrot.jbcrypt.BCrypt;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class SQLUserDao implements UserDAO {
     public SQLUserDao() throws DataAccessException {
         DatabaseManager.createTables();
@@ -14,7 +18,20 @@ public class SQLUserDao implements UserDAO {
         DatabaseManager.executeUpdate(statement, userData.username(), hash, userData.email());
     }
 
-    public UserData getUser(String username) {
+    public UserData getUser(String username) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT username, password, email FROM users WHERE username=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setNString(1, username);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return new UserData(rs.getString("username"), rs.getString("password"), rs.getString("email"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(String.format("Unable to read data: %s", e.getMessage()));
+        }
         return null;
     }
 
