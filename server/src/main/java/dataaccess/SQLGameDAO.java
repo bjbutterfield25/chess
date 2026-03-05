41b29b4c6b3f591f6d9db74ef9b63a23d0ddf0de
bjbutterfield25 <194412712+bjbutterfield25@users.noreply.github.com>
2026-03-05 13:22:09 -0700

    Implemented createGame method with sql

---
+++ server/src/main/java/dataaccess/SQLGameDAO.java
@@ -0,0 +1,34 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import model.GameData;
+
+import java.util.ArrayList;
+
+public class SQLGameDAO implements GameDAO {
+    public SQLGameDAO() throws DataAccessException {
+        DatabaseManager.createTables();
+    }
+
+    public void createGame(GameData gameData) throws DataAccessException {
+        var statement = "INSERT INTO games (whiteUsername, blackUsername, gameName, game) VALUES (?, ?, ?, ?)";
+        var game = new Gson().toJson(gameData.game());
+        DatabaseManager.executeUpdate(statement, gameData.whiteUsername(), gameData.blackUsername(), gameData.gameName(), game);
+    }
+
+    public GameData getGame(int gameID) {
+        return null;
+    }
+
+    public void deleteGame(int gameID) {
+
+    }
+
+    public ArrayList<GameData> listGames() {
+        return null;
+    }
+
+    public void clear() {
+
+    }
+}
