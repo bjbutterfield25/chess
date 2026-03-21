c74eed6de22872810590d2a106a25f1a63440bba
bjbutterfield25 <194412712+bjbutterfield25@users.noreply.github.com>
2026-03-21 16:00:01 -0600

    Got the non logged in REPL setup

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -5,8 +5,7 @@ import ui.Client;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-        Client.run();
+        System.out.println("♕ 240 Chess Client: ");
+        new Client().run();
     }
 }
--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,13 +1,49 @@
 package ui;
 
+import exceptions.ResponseException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Client {
     private static boolean isSignedIn;
-    public static void run() {
+    public void run() {
         System.out.println("Welcome to CS240 Chess. Type Help to get Started");
-        System.out.print(help());
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
     }
 
-    public static String help() {
+    private void printPrompt() {
+        System.out.print("\n" + ">>> ");
+    }
+
+    public String eval(String input) {
+        //try {
+            String[] tokens = input.toLowerCase().split(" ");
+            String cmd = (tokens.length > 0) ? tokens[0] : "help";
+            String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "quit" -> "quit";
+                default -> help();
+            };
+        //} catch (ResponseException ex) {
+            //return ex.getMessage();
+        //}
+    }
+
+    public String help() {
         if (!isSignedIn) {
             return """
                     - register - creates an account with username password and email
